1eb66d3d1b43b00e6b001b4456a6c8e6c16a00df
Donghyun <markk628@gmail.com>
2019-10-01 10:39:28 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    # return 'Hello, World!'
+    return render_template('home.html', msg='Flask is cool!!')
+if __name__  == '__main__':
+    app.run(debug=True)
